710f96c0a890a1f3e1897350a898d075ed2aaead
Harry Chappell <harrychapp@me.com>
2025-06-04 22:11:12 +0100

    Add film statistics functionality with JSON output and shortcodes

--- functions.php
+++ functions.php
@@ -1351,3 +1351,4 @@ add_shortcode('prediction_count', 'prediction_count_shortcode');
 
 
 require_once get_stylesheet_directory() . '/update_meta.php';
+require_once get_stylesheet_directory() . '/film_stats.php';
