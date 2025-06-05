34db7ba4da97d6fe2f18b279d5969a3c36602da9
Harry Chappell <harrychapp@me.com>
2025-06-05 20:36:04 +0100

    Add shortcode to return the number of users from all_user_stats.json

--- film_stats.php
+++ film_stats.php
@@ -401,3 +401,21 @@ function oscars_predictions_leaderboard_inner() {
     }
     return $output;
 }
+
+/**
+ * Returns the number of users in all_user_stats.json.
+ * @return int|false Number of users, or false if file missing/invalid.
+ */
+function oscars_get_user_count_shortcode() {
+    $output_path = ABSPATH . 'wp-content/uploads/all_user_stats.json';
+    if (!file_exists($output_path)) {
+        return false;
+    }
+    $json = file_get_contents($output_path);
+    $users = json_decode($json, true);
+    if (!$users || !is_array($users)) {
+        return false;
+    }
+    return count($users);
+}
+add_shortcode('oscars_get_user_count', 'oscars_get_user_count_shortcode');
