77897f8f15bd53a1b0276967131b935822be2849
Harry Chappell <harrychapp@me.com>
2025-06-11 19:56:04 +0100

    Update nominee page to write to watched json file.

--- nominees_nominations.php
+++ nominees_nominations.php
@@ -200,16 +200,24 @@ function nominees_nominations_function() {
             $year = get_the_date('Y'); // Get the year from the post's publish date
             
             echo '<li class="';
-            // Display categories without the "Category: " prefix and link them to their respective pages
             foreach ($categories as $category) {
                 echo ' ' . esc_html($category->slug);
             }
             echo ' nominee-visibility-' . $nominee_visibility;
 
-            if (is_user_logged_in()) {
-                $post_id = $film->term_id;
-                // Original line to get user's watched status
-                $user_watched = get_user_meta(get_current_user_id(), 'watched_' . $post_id, true);
+            $user_watched = false;
+            $post_id = ($film && isset($film->term_id)) ? $film->term_id : null;
+            if (is_user_logged_in() && $post_id) {
+                $user_id = get_current_user_id();
+                $json_path = ABSPATH . 'wp-content/uploads/user_meta/user_' . $user_id . '.json';
+                if (file_exists($json_path)) {
+                    $json_data = file_get_contents($json_path);
+                    $user_meta = json_decode($json_data, true);
+                    if (isset($user_meta['watched']) && is_array($user_meta['watched'])) {
+                        // Use loose comparison in case IDs are stored as strings
+                        $user_watched = in_array((string)$post_id, array_map('strval', $user_meta['watched']), true);
+                    }
+                }
                 if ($user_watched) {
                     echo ' watched ';
                 }
@@ -286,18 +294,24 @@ function nominees_nominations_function() {
             echo '</div>';
 
             if (is_user_logged_in()) {
+                $user_id = get_current_user_id();
                 $post_id = $film->term_id;
-                $user_watched = get_user_meta(get_current_user_id(), 'watched_' . $post_id, true);
-                
+                $json_path = ABSPATH . 'wp-content/uploads/user_meta/user_' . $user_id . '.json';
+                $user_watched = false;
+                if (file_exists($json_path)) {
+                    $json_data = file_get_contents($json_path);
+                    $user_meta = json_decode($json_data, true);
+                    if (isset($user_meta['watched']) && is_array($user_meta['watched'])) {
+                        $user_watched = in_array((string)$post_id, array_map('strval', $user_meta['watched']), true);
+                    }
+                }
                 $button_class = $user_watched ? 'mark-as-unwatched-button' : 'mark-as-watched-button';
                 $button_text = $user_watched ? 'Watched' : 'Unwatched';
                 $watched_action = $user_watched ? 'unwatched' : 'watched';
-                
                 echo '<button class="' . $button_class . '" data-film-id="' . $post_id . '" data-action="' . $watched_action . '">' . $button_text;
                 echo '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg>';
                 echo '</button>';
-            } else {
-                $post_id = $film->term_id;
+            } else if ($post_id) {
                 $button_class = 'mark-as-watched-button';
                 $button_text = 'Unwatched';
                 $watched_action = 'watched';
